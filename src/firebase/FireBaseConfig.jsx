266cf3582cfbab62c5ef0176b4097e7c54fc3a4c
nandu031105 <nandu031105@gmail.com>
2026-04-12 14:04:54 +0530

    ......

--- src/firebase/FireBaseConfig.jsx
+++ src/firebase/FireBaseConfig.jsx
@@ -3,14 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "authexample-19c32.firebaseapp.com",
-  projectId: "authexample-19c32",
-  storageBucket: "authexample-19c32.firebasestorage.app",
-  messagingSenderId: "621589089847",
-  appId: "1:621589089847:web:f03c45ee3010416c8af9b2",
-  measurementId: "G-Q0Q7Y37G0B"
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
